b1955833d3d3610011c810c3913bf37817eabdc7
Syed Haris <sydhrs@gmail.com>
2023-11-11 14:49:37 +0500

    structured app meta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Join from "./components/Join/Join";
+import Plans from "./components/Plans/Plans";
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
+import Testimonial from "./components/Testimonials/Testimonial";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Hero />
+        <Programs />
+        <Reasons />
+        <Plans />
+        <Testimonial />
+        <Join />
+        <Footer/>
+      </div>
   );
 }
 
